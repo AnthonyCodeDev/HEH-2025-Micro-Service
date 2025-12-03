8093fe6c87a15db434dd9d9f17c903dc10d60a4a
KaiZenKos <bodjonak.kevin@gmail.com>
2025-12-03 16:02:31 +0100

    Feat:Ajout du model/controller/service pour mon premier microservice (recommendation-service)

---
+++ recommendation-service/src/main/java/be/heh/recommendationservice/service/RecommendationService.java
@@ -0,0 +1,16 @@
+package main.java.be.heh.recommendationservice.service;
+
+import main.java.be.heh.recommendationservice.model.Recommendation;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class RecommendationService {
+    public Flux<Recommendation> getRecommendations(int productId) {
+        return Flux.just(
+                new Recommendation(productId, 1, "Kevin", 5, "Excellent produit !"),
+                new Recommendation(productId, 2, "Alice", 4, "Très bon mais améliorable."),
+                new Recommendation(productId, 3, "Marc", 3, "Correct sans plus.")
+        );
+    }
+}
