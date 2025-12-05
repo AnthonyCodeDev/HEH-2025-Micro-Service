0ee65780dcfb1060df2baa8317dfc2612aed814f
KaiZenKos <bodjonak.kevin@gmail.com>
2025-12-05 08:59:42 +0100

    Fix: Eureka server

--- recommendation-service/src/test/java/be/heh/recommendationservice/RecommendationServiceApplicationTests.java
+++ recommendation-service/src/test/java/be/heh/recommendationservice/RecommendationServiceApplicationTests.java
@@ -3,7 +3,7 @@ package be.heh.recommendationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration")
 class RecommendationServiceApplicationTests {
 
     @Test
