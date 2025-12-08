07b4cff946453a4010cbaeba5da69e1de214b168
KaiZenKos <bodjonak.kevin@gmail.com>
2025-12-08 09:34:26 +0100

    Feat: k8s

--- review-service/src/test/java/be/heh/reviewservice/ReviewServiceApplicationTests.java
+++ review-service/src/test/java/be/heh/reviewservice/ReviewServiceApplicationTests.java
@@ -3,7 +3,12 @@ package be.heh.reviewservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.r2dbc.url=r2dbc:h2:mem:///testdb;MODE=MYSQL",
+    "spring.r2dbc.username=sa",
+    "spring.r2dbc.password=",
+    "spring.sql.init.mode=never"
+})
 class ReviewServiceApplicationTests {
 
     @Test
