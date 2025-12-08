b6870c4eb35daccf96ed2dbc7e7c36781ce66cad
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-12-08 08:45:40 +0100

    Eureka Removed

--- recommendation-service/src/main/java/be/heh/recommendationservice/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/be/heh/recommendationservice/RecommendationServiceApplication.java
@@ -2,10 +2,8 @@ package be.heh.recommendationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class RecommendationServiceApplication {
 
     public static void main(String[] args) {
